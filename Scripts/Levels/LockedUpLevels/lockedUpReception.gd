5d1b9ee9db0f44d30390bcfb1601e3508ac939a0
Gianfranco Cicciomessere <cicciomesere@gmail.com>
2023-01-16 11:52:15 +0000

    Cleaning code + cooldown function

--- Scripts/Levels/LockedUpLevels/lockedUpReception.gd
+++ Scripts/Levels/LockedUpLevels/lockedUpReception.gd
@@ -5,28 +5,40 @@ var time = 0
 var errors = 0
 onready var allCameras =  $AllCameras.get_children()
 
+# Built in _functions
 
 func _ready():
 	$GUI/OnPC/IncidentMenu.visible = false
 	$GUI/OnPC/AnomalyDetected.visible = false
+	$GUI/OnPC/IncidentMenu/Panel/CooldownCounter.visible = false
 	onLobby()
 	
 func _process(delta):
 	currentCamera = get_viewport().get_camera()
 	$GUI/OnPC/CurrentCamera.text = currentCamera.name
+	processTime(delta)
+
+# My own Functions
 	
-	if time / 10 > 10:
-		$GUI/OnPC/Time.text = "22:%s" % str(int(time / 10))
-	else:
-		$GUI/OnPC/Time.text = "22:0%s" % str(int(time / 10))
-	time += 1 * delta
-
-func _on_Player_canUsePC(status):
-	if status:
-		$GUI/Normal/UsePC.visible = true
-	else:
-		$GUI/Normal/UsePC.visible = false
-
+func incorrectAnomalyReported():
+	errors += 1
+	if errors == 1:
+		$Structures/Lobby/MaletaArriba/Animacion.play("Fase1")
+	elif errors == 2:
+		$Structures/Lobby/MaletaArriba/Animacion.play("Fase2")
+	elif errors == 3:
+		$Structures/Lobby/MaletaArriba/Animacion.play("Fase3")
+	elif errors == 4:
+		$Structures/Lobby/MaletaArriba/Animacion.play("Fase4")
+	elif errors == 5:
+		$Structures/Lobby/MaletaArriba/Animacion.play("Fase5")
+		$Structures/Lobby/Godot.visible = false
+	$GUI/OnPC/IncidentMenu/Panel/Anomaly1.disabled = true
+	$GUI/OnPC/IncidentMenu/Panel/Anomaly2.disabled = true
+	$GUI/OnPC/IncidentMenu/Panel/Anomaly3.disabled = true
+	$GUI/OnPC/IncidentMenu/Panel/CooldownCounter.visible = true
+	$AnomalyCooldown.start()
+	
 func onUsingPC():
 	$GUI/Normal.visible = false
 	$GUI/OnPC.visible = true
@@ -34,7 +46,36 @@ func onUsingPC():
 func onLobby():
 	$GUI/Normal.visible = true
 	$GUI/OnPC.visible = false
+	
+func processTime(delta):
+	if time / 10 > 10:
+		$GUI/OnPC/Time.text = "22:%s" % str(int(time / 10))
+	else:
+		$GUI/OnPC/Time.text = "22:0%s" % str(int(time / 10))
+	time += 1 * delta
+	
+func cooldownCounter():
+	pass
 
+# Signal Functions
+
+func _on_Anomaly1_pressed():
+	incorrectAnomalyReported()
+
+
+func _on_Anomaly2_pressed():
+	incorrectAnomalyReported()
+
+
+func _on_Anomaly3_pressed():
+	incorrectAnomalyReported()
+
+
+func _on_AnomalyCooldown_timeout():
+	$GUI/OnPC/IncidentMenu/Panel/Anomaly1.disabled = false
+	$GUI/OnPC/IncidentMenu/Panel/Anomaly2.disabled = false
+	$GUI/OnPC/IncidentMenu/Panel/Anomaly3.disabled = false
+	$GUI/OnPC/IncidentMenu/Panel/CooldownCounter.visible = false
 
 func _on_UsePC_button_up():
 	$GUI/Normal.visible = false
@@ -69,26 +110,15 @@ func _on_GoLeft_button_up():
 	else:
 		allCameras[allCameras.size() - 1].current = true
 
-
 func _on_Report_pressed():
 	if $GUI/OnPC/IncidentMenu.visible == false:
 		$GUI/OnPC/IncidentMenu.visible = true
 
-
 func _on_LeaveIncident_pressed():
 	$GUI/OnPC/IncidentMenu.visible = false
 
-
-func _on_Anomaly1_pressed():
-	errors += 1
-	if errors == 1:
-		$Structures/Lobby/MaletaArriba/Animacion.play("Fase1")
-	elif errors == 2:
-		$Structures/Lobby/MaletaArriba/Animacion.play("Fase2")
-	elif errors == 3:
-		$Structures/Lobby/MaletaArriba/Animacion.play("Fase3")
-	elif errors == 4:
-		$Structures/Lobby/MaletaArriba/Animacion.play("Fase4")
-	elif errors == 5:
-		$Structures/Lobby/MaletaArriba/Animacion.play("Fase5")
-		$Structures/Lobby/Godot.visible = false
+func _on_Player_canUsePC(status):
+	if status:
+		$GUI/Normal/UsePC.visible = true
+	else:
+		$GUI/Normal/UsePC.visible = false
